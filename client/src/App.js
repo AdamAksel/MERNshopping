00ae1f8e4b9fcae9da1f6d3d53866478ff74fcd3
AdamAksel <akseltheaustrian@gmail.com>
2020-10-22 17:06:43 +0700

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen';
+import Productscreen from './screens/Productscreen';
 
 function App() {
   return (
-    <React.Fragment>
-      <h1>Welcome to the Store</h1>
-    </React.Fragment>
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={Homescreen} exact />
+          <Route path='/product/:id' component={Productscreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 

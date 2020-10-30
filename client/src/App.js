43131ce841f095b86f63ee876c605ee24a9f4623
AdamAksel <akseltheaustrian@gmail.com>
2020-10-30 21:42:46 +0700

    fifth commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Homescreen from './screens/Homescreen';
-import Productscreen from './screens/Productscreen';
-import Cartscreen from './screens/Cartscreen';
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Homescreen from './screens/Homescreen'
+import Productscreen from './screens/Productscreen'
+import Cartscreen from './screens/Cartscreen'
+import Loginscreen from './screens/Loginscreen'
+import Registerscreen from './screens/Registerscreen'
+import Profilescreen from './screens/Profilescreen'
+import Shippingscreen from './screens/Shippingscreen'
 
 function App() {
   return (
@@ -15,14 +19,18 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={Homescreen} exact />
+          <Route path='/login' component={Loginscreen} />
+          <Route path='/shipping' component={Shippingscreen} />
+          <Route path='/register' component={Registerscreen} />
+          <Route path='/profile' component={Profilescreen} />
           <Route path='/product/:id' component={Productscreen} />
           <Route path='/cart/:id?' component={Cartscreen} />
+          <Route path='/' component={Homescreen} exact />
         </Container>
       </main>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,11 +11,14 @@ const app = express();
 
 connectDB();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
